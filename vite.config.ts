fb0fedf7b2d37addc2880e880733ccb640a058b2
Marcelle Medeiros <112626750+cellemedeiros@users.noreply.github.com>
2024-12-23 14:10:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,23 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => ({
   server: {
-    port: 3000,
+    host: "::",
+    port: 8080,
   },
-  optimizeDeps: {
-    exclude: ["some-package"],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  esbuild: {
-    loader: "tsx",
-  },
-});
+}));
 
